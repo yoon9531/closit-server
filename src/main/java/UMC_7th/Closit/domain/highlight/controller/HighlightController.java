4327f6ae367d3873d304e3310042963974b734a4
jungh150 <jungh150@naver.com>
2025-02-20 22:59:48 +0900

    #165 fix: 하이라이트 목록 조회 api 수정

--- src/main/java/UMC_7th/Closit/domain/highlight/controller/HighlightController.java
+++ src/main/java/UMC_7th/Closit/domain/highlight/controller/HighlightController.java
@@ -18,21 +18,12 @@ import org.springframework.web.bind.annotation.*;
 public class HighlightController {
 
     private final HighlightCommandService highlightCommandService;
-    private final HighlightQueryService highlightQueryService;
 
     @Operation(summary = "하이라이트 생성", description = "새로운 하이라이트를 생성합니다.")
     @PostMapping
     public ApiResponse<HighlightResponseDTO.CreateHighlightResultDTO> createHighlight(@RequestBody @Valid HighlightRequestDTO.CreateHighlightDTO request) {
         Highlight highlight = highlightCommandService.createHighlight(request);
         return ApiResponse.onSuccess(HighlightConverter.toCreateHighlightResultDTO(highlight));
-    }
-
-    @Operation(summary = "하이라이트 상세 조회", description = "ID를 통해 특정 하이라이트의 상세 정보를 조회합니다.")
-    @GetMapping("/{highlight_id}")
-    public ApiResponse<HighlightResponseDTO.HighlightDetailDTO> getHighlightById(@PathVariable Long highlight_id) {
-        Highlight highlight = highlightQueryService.findHighlight(highlight_id);
-        HighlightResponseDTO.HighlightDetailDTO detailDTO = HighlightConverter.toHighlightDetailDTO(highlight);
-        return ApiResponse.onSuccess(detailDTO);
     }
 
     @Operation(summary = "하이라이트 삭제", description = "ID를 통해 특정 하이라이트를 삭제합니다.")
