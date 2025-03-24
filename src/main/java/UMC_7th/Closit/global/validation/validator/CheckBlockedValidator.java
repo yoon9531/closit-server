6345c4f5cdf6f0b5438f8090b2d1c2c2b1f8bea1
seongyoon <dlatjddbs9844@yonsei.ac.kr>
2025-03-24 09:48:18 +0900

    fix: 불필요한 로그 삭제

--- src/main/java/UMC_7th/Closit/global/validation/validator/CheckBlockedValidator.java
+++ src/main/java/UMC_7th/Closit/global/validation/validator/CheckBlockedValidator.java
@@ -1,6 +1,5 @@
 package UMC_7th.Closit.global.validation.validator;
 
-import UMC_7th.Closit.domain.user.repository.UserRepository;
 import UMC_7th.Closit.domain.user.service.UserCommandService;
 import UMC_7th.Closit.global.validation.annotation.CheckBlocked;
 import UMC_7th.Closit.security.SecurityUtil;
@@ -11,7 +10,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -31,7 +29,7 @@ public class CheckBlockedValidator {
         String requesterClositId = securityUtil.getCurrentUser().getClositId();
         // 차단 여부 검사
         if (userService.isBlockedBy(targetClositId, requesterClositId)) {
-            log.warn("차단된 사용자 접근 차단: [{}] is blocked by [{}]", requesterClositId, targetClositId);
+//            log.warn("차단된 사용자 접근 차단: [{}] is blocked by [{}]", requesterClositId, targetClositId);
             throw new AccessDeniedException("You are blocked by this user.");
         }
     }
