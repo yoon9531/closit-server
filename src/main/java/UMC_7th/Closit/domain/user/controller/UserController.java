a228b101ec85c40edee63e9d3822818d8b6b88db
zyovn <yui2507@naver.com>
2025-03-26 16:50:18 +0900

    #188 minor: 사용자 관련 uri 일부 수정

--- src/main/java/UMC_7th/Closit/domain/user/controller/UserController.java
+++ src/main/java/UMC_7th/Closit/domain/user/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
     private String profileImagePath;
 
     @Operation(summary = "사용자 삭제", description = "특정 사용자를 삭제합니다.")
-    @DeleteMapping("/")
+    @DeleteMapping()
     public ApiResponse<String> deleteUser() {
 
         userCommandService.deleteUser();
@@ -43,7 +43,7 @@ public class UserController {
     }
 
     @Operation(summary = "사용자 정보 수정", description = "사용자 정보를 수정합니다.")
-    @PatchMapping("/")
+    @PatchMapping()
     public ApiResponse<UserResponseDTO.UserInfoDTO> updateUserInfo(@Valid @RequestBody UserRequestDTO.UpdateUserDTO updateUserDTO) {
 
         User userInfo = userCommandService.updateUserInfo(updateUserDTO);
