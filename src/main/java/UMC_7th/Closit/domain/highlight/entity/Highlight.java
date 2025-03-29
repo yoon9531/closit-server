33a620799e22c28af7bd7dbb63e4e30ea71704ee
Junghwa <jungh150@naver.com>
2025-03-30 01:15:34 +0900

    #201 refactor: highlight 엔티티에서 user 삭제

--- src/main/java/UMC_7th/Closit/domain/highlight/entity/Highlight.java
+++ src/main/java/UMC_7th/Closit/domain/highlight/entity/Highlight.java
@@ -22,10 +22,6 @@ public class Highlight extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 }
