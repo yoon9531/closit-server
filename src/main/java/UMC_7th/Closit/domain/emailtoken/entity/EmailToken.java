e0fce75cbeb535f233d44f92878815b2ef86f6bb
jungh150 <jungh150@naver.com>
2025-03-30 17:30:10 +0900

    #207 feat: EmailToken 엔티티 생성

---
+++ src/main/java/UMC_7th/Closit/domain/emailtoken/entity/EmailToken.java
@@ -0,0 +1,33 @@
+package UMC_7th.Closit.domain.emailtoken.entity;
+
+import UMC_7th.Closit.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class EmailToken extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "email_token_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expiredAt;
+
+    @Column(nullable = false)
+    private boolean used;
+}
