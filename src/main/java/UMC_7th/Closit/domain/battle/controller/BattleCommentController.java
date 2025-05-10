1833c06a9e4060b78f7c0fb3acb7ea5c6d53f80d
zyovn <yui2507@naver.com>
2025-05-11 04:20:52 +0900

    #225 refactor: 배틀 댓글 api endpoint 수정

--- src/main/java/UMC_7th/Closit/domain/battle/controller/BattleCommentController.java
+++ src/main/java/UMC_7th/Closit/domain/battle/controller/BattleCommentController.java
@@ -10,21 +10,23 @@ import UMC_7th.Closit.domain.user.entity.User;
 import UMC_7th.Closit.global.apiPayload.ApiResponse;
 import UMC_7th.Closit.security.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "[배틀 게시판 댓글]", description = "배틀 게시판 댓글 관련 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth/communities/battle")
+@RequestMapping("/api/v1/communities/battle")
 public class BattleCommentController {
 
     private final BattleCmtCommandService battleCmtCommandService;
     private final BattleCmtQueryService battleCmtQueryService;
     private final SecurityUtil securityUtil;
 
-    @PostMapping("/{battle_id}/comments")
+    @PostMapping("/{battleId}/comments")
     @Operation(summary = "새로운 배틀 댓글 생성",
             description = """
             ## 배틀 게시글에 댓글 작성
@@ -35,7 +37,7 @@ public class BattleCommentController {
             parentCommentId [부모 댓글 ID - 대댓글일 경우에만 요청]
             """)
     public ApiResponse<BattleCommentResponseDTO.CreateBattleCommentDTO> createBattleComment(@RequestBody @Valid BattleCommentRequestDTO.CreateBattleCommentDTO request,
-                                                                                            @PathVariable("battle_id") Long battleId) {
+                                                                                            @PathVariable("battleId") Long battleId) {
 
         User user = securityUtil.getCurrentUser();
         Long userId = user.getId();
@@ -45,7 +47,7 @@ public class BattleCommentController {
         return ApiResponse.onSuccess(BattleCommentConverter.createBattleCommentResponseDTO(battleComment));
     }
 
-    @GetMapping("/{battle_id}/comments")
+    @GetMapping("/{battleId}/comments")
     @Operation(summary = "배틀 댓글 조회",
             description = """
             ## 배틀 게시글 댓글 목록 조회
@@ -54,7 +56,7 @@ public class BattleCommentController {
             ### Parameters
             page [조회할 페이지 번호] - 0부터 시작, 10개씩 보여줌
             """)
-    public ApiResponse<BattleCommentResponseDTO.BattleCommentPreviewListDTO> getBattleComments(@PathVariable("battle_id") Long battleId,
+    public ApiResponse<BattleCommentResponseDTO.BattleCommentPreviewListDTO> getBattleComments(@PathVariable("battleId") Long battleId,
                                                                                                @RequestParam(name = "page") Integer page) {
 
         Slice<BattleComment> battleCommentList = battleCmtQueryService.getBattleCommentList(battleId, page);
@@ -62,16 +64,16 @@ public class BattleCommentController {
         return ApiResponse.onSuccess(BattleCommentConverter.battleCommentPreviewListDTO(battleCommentList));
     }
 
-    @DeleteMapping("/{battle_id}/comments/{battle_comment_id}")
+    @DeleteMapping("/{battleId}/comments/{battleCommentId}")
     @Operation(summary = "배틀 댓글 삭제",
             description = """
             ## 배틀 게시글 내 특정 댓글 삭제
             ### PathVariable
-            battle_id [배틀 ID] \n
-            battle_comment_id [배틀 댓글 ID]
+            battleId [배틀 ID] \n
+            battleCommentId [배틀 댓글 ID]
             """)
-    public ApiResponse<String> deleteBattleComment(@PathVariable("battle_id") Long battleId,
-                                                   @PathVariable("battle_comment_id") Long battleCommentId) {
+    public ApiResponse<String> deleteBattleComment(@PathVariable("battleId") Long battleId,
+                                                   @PathVariable("battleCommentId") Long battleCommentId) {
 
         User user = securityUtil.getCurrentUser();
         Long userId = user.getId();
