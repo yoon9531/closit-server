7cd35fa3f19c2bf6973730ab2c97861928eb1439
zyovn <yui2507@naver.com>
2025-05-12 01:35:29 +0900

    #225 fix: 조회순 trending -> view로 수정

--- src/main/java/UMC_7th/Closit/domain/battle/controller/BattleController.java
+++ src/main/java/UMC_7th/Closit/domain/battle/controller/BattleController.java
@@ -128,7 +128,7 @@ public class BattleController {
             ## 배틀 게시글 목록 조회 - 투표 하지 않은 배틀 게시글은 0으로 보임
             ### Parameters
             page [조회할 페이지 번호] - 0부터 시작, 10개씩 보여줌 \n
-            sorting [배틀 정렬 방식] - 최신순 (LATEST), 인기순 (TRENDING) \n
+            sorting [배틀 정렬 방식] - 최신순 (LATEST), 조회순 (VIEW) \n
             status [배틀 진행 상태] - 진행 중 (ACTIVE), 종료 (COMPLETED)
             """)
     public ApiResponse<BattleResponseDTO.BattlePreviewListDTO> getBattleList(@RequestParam(name = "page") Integer page,
