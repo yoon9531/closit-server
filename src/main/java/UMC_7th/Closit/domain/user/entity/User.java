dc07420f5a0a272f375689088482d76483a7a6be
zyovn <yui2507@naver.com>
2025-06-24 15:35:44 +0900

    #203 feat: user-fcmToken 연관관계 설정

--- src/main/java/UMC_7th/Closit/domain/user/entity/User.java
+++ src/main/java/UMC_7th/Closit/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import UMC_7th.Closit.domain.battle.entity.BattleLike;
 import UMC_7th.Closit.domain.battle.entity.Vote;
 import UMC_7th.Closit.domain.follow.entity.Follow;
 import UMC_7th.Closit.domain.notification.entity.Notification;
+import UMC_7th.Closit.domain.notification.firebase.domain.FcmToken;
 import UMC_7th.Closit.domain.post.entity.Bookmark;
 import UMC_7th.Closit.domain.post.entity.Comment;
 import UMC_7th.Closit.domain.post.entity.Likes;
@@ -107,6 +108,10 @@ public class User extends BaseEntity {
     @Builder.Default
     private List<Notification> notificationList = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<FcmToken> fcmTokenList = new ArrayList<>();
+
     public User updateRole(Role newRole) {
         this.role = newRole;
         return this;
@@ -124,7 +129,6 @@ public class User extends BaseEntity {
     public void setClositId(@Size(min = 2, max = 20, message = "clositId는 2~20자 사이여야 합니다.") String clositId) {
         this.clositId = clositId;
     }
-
 
     public void incrementCountReport() {
         this.countReport++;
