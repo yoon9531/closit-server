11991650f4bae2ffa5dc32311977a75ade28b936
Yeonnies <eliyeon16@gmail.com>
2025-07-01 17:11:37 +0900

    #282 feat: 좋아요 목록 조회 api 구현

--- src/main/java/UMC_7th/Closit/domain/post/repository/LikeRepository.java
+++ src/main/java/UMC_7th/Closit/domain/post/repository/LikeRepository.java
@@ -3,6 +3,8 @@ package UMC_7th.Closit.domain.post.repository;
 import UMC_7th.Closit.domain.post.entity.Likes;
 import UMC_7th.Closit.domain.post.entity.Post;
 import UMC_7th.Closit.domain.user.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,6 @@ public interface LikeRepository extends JpaRepository<Likes, Long> {
     boolean existsByUserAndPost(User user, Post post);
     
     Optional<Likes> findByUserAndPost(User user, Post post);
+
+    Slice<Likes> findByPost(Post post, Pageable pageable);
 }
--- src/main/java/UMC_7th/Closit/domain/post/service/LikeService.java
+++ src/main/java/UMC_7th/Closit/domain/post/service/LikeService.java
@@ -1,8 +1,10 @@
 package UMC_7th.Closit.domain.post.service;
 
 import UMC_7th.Closit.domain.post.dto.LikeResponseDTO;
+import org.springframework.data.domain.Pageable;
 
 public interface LikeService {
     LikeResponseDTO.LikeStatusDTO likePost(Long postId);
     LikeResponseDTO.LikeStatusDTO unlikePost(Long postId);
+    LikeResponseDTO.LikedUserListDTO getLikedUsers(Long postId, Pageable pageable);
 }
