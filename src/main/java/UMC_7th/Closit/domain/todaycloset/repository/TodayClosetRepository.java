b35d9b5573e648a96552cea6a80c6cd1b80d05b9
Yeonnies <eliyeon16@gmail.com>
2025-07-04 02:10:42 +0900

    #288 fix: 오늘의 옷장 api 수정

--- src/main/java/UMC_7th/Closit/domain/todaycloset/repository/TodayClosetRepository.java
+++ src/main/java/UMC_7th/Closit/domain/todaycloset/repository/TodayClosetRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 
 public interface TodayClosetRepository extends JpaRepository<TodayCloset, Long> {
     Page<TodayCloset> findAll(Pageable pageable);
@@ -16,4 +19,31 @@ public interface TodayClosetRepository extends JpaRepository<TodayCloset, Long> 
     Slice<TodayCloset> findAllOrderByPostView(Pageable pageable);
     @Query("SELECT tc FROM TodayCloset tc ORDER BY tc.createdAt DESC")
     Slice<TodayCloset> findAllOrderByCreatedAt(Pageable pageable);
+
+    // 조회수 순 정렬 (24시간 이내)
+    @Query("""
+    SELECT tc 
+    FROM TodayCloset tc 
+    JOIN tc.post p 
+    WHERE tc.createdAt BETWEEN :start AND :end 
+    ORDER BY p.view DESC
+    """)
+    Slice<TodayCloset> findTodayClosetsOrderByPostView(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            Pageable pageable
+    );
+
+    // 최신순 정렬 (24시간 이내)
+    @Query("""
+    SELECT tc 
+    FROM TodayCloset tc 
+    WHERE tc.createdAt BETWEEN :start AND :end 
+    ORDER BY tc.createdAt DESC
+    """)
+    Slice<TodayCloset> findTodayClosetsByCreatedAtBetween(
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end,
+            Pageable pageable
+    );
 }
