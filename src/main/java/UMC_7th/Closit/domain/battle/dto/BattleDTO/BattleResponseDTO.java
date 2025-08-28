b30badfeeeb42a9103176e355eaf3a126b7f5d1b
zyovn <yui2507@naver.com>
2025-08-28 17:13:18 +0900

    #305 refactor: 배틀 조회 response 리팩토링

--- src/main/java/UMC_7th/Closit/domain/battle/dto/BattleDTO/BattleResponseDTO.java
+++ src/main/java/UMC_7th/Closit/domain/battle/dto/BattleDTO/BattleResponseDTO.java
@@ -1,14 +1,10 @@
 package UMC_7th.Closit.domain.battle.dto.BattleDTO;
 
-import UMC_7th.Closit.domain.battle.entity.enums.BattleStatus;
-import UMC_7th.Closit.domain.battle.entity.enums.ChallengeStatus;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class BattleResponseDTO {
@@ -72,27 +68,5 @@ public class BattleResponseDTO {
         private boolean isFirst;
         private boolean isLast;
         private boolean hasNext;
-    }
-
-    @Builder
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class GetChallengeBattleDTO {
-        private Long battleId;
-        private Long challengeBattleId;
-        private String title;
-        private String description;
-        private String firstClositId;
-        private String firstProfileImage;
-        private Long firstPostId;
-        private String firstPostFrontImage;
-        private String firstPostBackImage;
-        private String secondClositId;
-        private String secondProfileImage;
-        private Long secondPostId;
-        private String secondPostFrontImage;
-        private String secondPostBackImage;
-        private ChallengeStatus challengeStatus;
     }
 }
---
+++ src/main/java/UMC_7th/Closit/domain/battle/dto/BattleDTO/response/ChallengeBattleListResponse.java
@@ -0,0 +1,24 @@
+package UMC_7th.Closit.domain.battle.dto.BattleDTO.response;
+
+import lombok.Builder;
+
+@Builder
+public record ChallengeBattleListResponse(
+        Long battleId,
+        boolean isLiked,
+        int likeCount,
+        String title,
+        String firstClositId,
+        String firstProfileImage,
+        Long firstPostId,
+        String firstPostFrontImage,
+        String firstPostBackImage,
+        int firstVotingCnt,
+        String secondClositId,
+        String secondProfileImage,
+        Long secondPostId,
+        String secondPostFrontImage,
+        String secondPostBackImage,
+        int secondVotingCnt
+) {
+}
